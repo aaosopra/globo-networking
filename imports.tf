dcf5e26d6e7eaeaa1fd4da3ba5cbfab6008afb91
aao <aao@AAP-5CG208925V>
2024-08-07 13:27:24 +0200

    init

---
+++ imports.tf
@@ -0,0 +1,48 @@
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-0193e0a2e0361974d" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-0ddb64288de92a32a" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-06702b70235075bff" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-01751c9a93f6a059f" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-051cf01ca7ff8cbdf_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-051cf01ca7ff8cbdf" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-0ddb64288de92a32a/rtb-051cf01ca7ff8cbdf" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-06702b70235075bff/rtb-051cf01ca7ff8cbdf" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-05282500775e97dee" #NoIngressSecurityGroup
+# }
